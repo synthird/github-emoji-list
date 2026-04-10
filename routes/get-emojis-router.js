e554d3302761538b559bb4bfdcdbd806fa09a21d
Synthird <105394020+Synthird@users.noreply.github.com>
2026-04-09 18:52:54 -0700

    Fetching the GitHub emojis is now done through the get emojis router rather than a controller/seperate JavaScript file

--- routes/get-emojis-router.js
+++ routes/get-emojis-router.js
@@ -1,7 +1,25 @@
 import express from "express";
-import { getEmojis } from "../controllers/get-emojis-controller.js";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+	auth: process.env.TOKEN
+});
 
 const router = express.Router();
-router.get("/", getEmojis);
+
+router.get("/", async (_, res) => {
+	try {
+		const result = await octokit.request("GET /emojis"),
+			emojis = result.data;
+
+		res.json(emojis);
+	} catch (error) {
+		if (error.response) {
+			res.status(error.response.status).json({ message: error.response.data.message });
+		} else {
+			console.log(error);
+		}
+	}
+});
 
 export default router;
